226abe5b37b0522fb2fd477202074eeca947b216
jizha264 <ji.zhang5@inter.ikea.com>
2022-12-07 22:44:23 +0100

    Pipeline enrichment: add etl python operator, value check, additional packages in the docker environment

--- dags/weather_etl.py
+++ dags/weather_etl.py
@@ -5,14 +5,14 @@ import requests
 from airflow import DAG
 from airflow.decorators import task
 from airflow.providers.postgres.operators.postgres import PostgresOperator
+from airflow.operators.python import PythonOperator
+from airflow.operators.check_operator import ValueCheckOperator
+from scripts.etl import etl
 
 ENV_ID = os.environ.get("SYSTEM_TESTS_ENV_ID")
 DAG_ID = "weather_etl"
 default_args = {
     "depends_on_past": False,
-    "email": ["jmutatiina@deloitte.nl"],
-    "email_on_failure": True,
-    "email_on_retry": False,
     "retries": 1,
     "retry_delay": timedelta(minutes=5),
 }
@@ -30,32 +30,28 @@ with DAG(
     create_weather_table = PostgresOperator(
             task_id="create_pet_table",
             sql="""
-                CREATE TABLE IF NOT EXISTS weather2 (
+                CREATE TABLE IF NOT EXISTS weather (
                 id SERIAL PRIMARY KEY,
-                weather_data JSON NOT NULL);
+                city text,
+                current_temperature numeric,
+                tomorrow_max_temperature numeric,
+                tomorrow_min_temperature numeric,
+                current_temperature_fahrenheit numeric);
             """,
         )
 
     # custom python operator
     # https://airflow.apache.org/docs/apache-airflow/stable/howto/operator/python.html#
-    @task
-    def get_current_weather(ti=None):
-        # Package the request, send the request and catch the response: r
-        request_url = "http://weerlive.nl/api/json-data-10min.php?key=d207adc652&locatie=Amsterdam"
-
-        parameters = {"t": "Current weather in Amsterdam"}
-
-        response = requests.get(request_url, params=parameters)
-        ti.xcom_push(key="current_weather", value=response.text)
-
-    source_current_weather = get_current_weather()
-
-    populate_weather_data = PostgresOperator(
-        task_id="populate_pet_table",
-        sql="""
-            INSERT INTO weather2 (weather_data) VALUES
-            (json('{{ti.xcom_pull(task_ids='get_current_weather', key='current_weather')}}'));
-           """
+    process_data = PythonOperator(
+        task_id='etl',
+        python_callable=etl
     )
 
-    create_weather_table >> source_current_weather >> populate_weather_data
+    check_city_count = ValueCheckOperator(
+        task_id='check_city_count',
+        sql="SELECT COUNT(DISTINCT(city)) FROM weather",
+        pass_value=1,
+        conn_id="postgres_default"
+    )
+
+    create_weather_table >> process_data >> check_city_count
